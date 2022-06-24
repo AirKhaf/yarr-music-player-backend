5d2f7377c793bff42c978ef5cf586f65fb472f1e
Airat Khafizov <airhaf1996@gmail.com>
2022-06-24 22:13:59 +0400

    init

---
+++ src/main/java/online/airkhavdev/yarrmusicplayerbackend/YarrMediaPlayerBackendApplication.java
@@ -0,0 +1,11 @@
+package online.airkhavdev.yarrmusicplayerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YarrMediaPlayerBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YarrMediaPlayerBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/online/airkhavdev/yarrmusicplayerbackend/YaarMediaPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package online.airkhavdev.yarrmusicplayerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaarMediaPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
